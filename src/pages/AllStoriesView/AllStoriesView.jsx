1fc4717787750bb848b091bb39bd54f3ddab1a3c
Monica-R <moniroka_339@hotmail.com>
2025-03-09 22:08:34 +0000

    feat: add AllStoriesView and EditProfileView components with basic structure and styling

---
+++ src/pages/AllStoriesView/AllStoriesView.jsx
@@ -0,0 +1,10 @@
+import './AllStoriesView.css';
+import React from 'react'
+
+function AllStoriesView() {
+  return (
+    <div>AllStoriesView</div>
+  )
+}
+
+export default AllStoriesView
---
+++ src/pages/EditProfileView/EditProfileView.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './EditProfileView.css';
+
+function EditProfileView() {
+  return (
+    <div>EditProfileView</div>
+  )
+}
+
+export default EditProfileView
