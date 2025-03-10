49868931a101736c44fd2a989ae41c4b8ba24dd8
Monica-R <moniroka_339@hotmail.com>
2025-03-10 17:42:30 +0000

    feat: add route for StoryDetailsView in App component

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import AllStoriesView from './pages/AllStoriesView/AllStoriesView'
 import MyStoriesView from './pages/MyStoriesView/MyStoriesView'
 import EditProfileView from './pages/EditProfileView/EditProfileView'
 import Layout from './layouts/Layout'
+import StoryDetailsView from './pages/StoryDetailsView/StoryDetailsView'
 
 function App() {
 
@@ -26,6 +27,7 @@ function App() {
               <Route path='/profile' element={ <ProfileView /> } />
               <Route path='/admin' element={ <Admin /> } />
               <Route path='/stories' element={ <AllStoriesView /> } />
+              <Route path='/stories/:id' element={ <StoryDetailsView /> } />
               <Route path='/stories/user' element={ <MyStoriesView /> } />
               <Route path='/edit-profile' element={ <EditProfileView /> } />
               <Route path='*' element={<NotFound />} />
