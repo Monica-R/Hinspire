686aa714db98f7d8773551b15a0f68a607ee8316
Monica-R <moniroka_339@hotmail.com>
2025-03-10 16:29:06 +0000

    change: signup & login buttons/links added, handleLogout added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../../context/auth.context';
 
 function Navbar() {
+  const { authToken, setAuthToken, user } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setAuthToken(null); //Borramos el token
+    navigate("/");
+  }
+
   return (
     <nav className='nav'>
-      <Link to="/login">Conectarse</Link>
-      <Link to="/signup">Registro</Link>
+      { authToken ? (
+        <>
+          <Link to="/profile">Hi, { user?.username }!</Link>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      ) : (
+        <>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">SignUp</Link>
+        </>
+      ) }
     </nav>
   )
 }
