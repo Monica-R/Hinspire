437739167d456bc8d76be7c57f52ce0f268a51de
Monica-R <moniroka_339@hotmail.com>
2025-03-12 00:10:49 +0000

    feat: add functions to fetch story by ID and create fragments

--- src/services/fragments.js
+++ src/services/fragments.js
@@ -2,7 +2,20 @@ import { api } from './config';
 
 export const getFragmentsOfStory = async (storyId) => {
     try {
-        const response = api.get(`/story/${storyId}`);
+        const response = await api.get(`fragments/story/${storyId}`);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const createFragment = async (storyId, fragment, token) => {
+    try {
+        const response = await api.post(`fragments/${storyId}`, { content: fragment },{
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        });
         return response.data;
     } catch (error) {
         console.error(error);
--- src/services/stories.js
+++ src/services/stories.js
@@ -9,8 +9,18 @@ export const fetchStories = async () => {
   }
 };
 
+export const fetchStoryById =  async (storyId) => {
+  try {
+    const storyRes = await api.get(`stories/${storyId}`);
+    return storyRes.data;
+  } catch (error) {
+    console.error('error', error);
+  }
+}
+
 export const fetchStoriesOfUser = async (userId) => {
   try {
+    console.log(userId);
     const token = localStorage.getItem("authToken");
     const storiesRes = await api.get(`stories/user/${userId}`, {
       headers: {
