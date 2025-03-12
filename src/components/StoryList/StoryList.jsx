e9d1a91318dc083cd5b69dd43bf3348cdcb5268e
Monica-R <moniroka_339@hotmail.com>
2025-03-12 17:51:20 +0000

    feat: refactor StoryList component to accept stories as props and remove unnecessary state management

--- src/components/StoryList/StoryList.jsx
+++ src/components/StoryList/StoryList.jsx
@@ -1,43 +1,10 @@
 import './StoryList.css';
 import React from 'react'
-import { useEffect, useState } from 'react'
-import { fetchStories, fetchStoriesOfUser } from '../../services/stories';
 import StoryItem from '../StoryItem/StoryItem';
-import { useAuth } from '../../context/auth.context';
-import { useLocation } from 'react-router-dom';
 
-function StoryList() {
+function StoryList({stories}) {
 
-    const [stories, setStories] = useState([]);
-    const { user } = useAuth();
-    const location = useLocation();
-
-    // Determina si estamos en la ruta de "Mis historias"
-    const isMyStoriesRoute = location.pathname.includes('/stories/user');
-
-    console.info('fullpath', location.pathname + location.search + location.hash);
-
-    useEffect(() => {
-        const getStories = async () => {
-            try {
-                let data;
-                if(isMyStoriesRoute) {
-                    data = user._id ? await fetchStoriesOfUser(user._id) : ["not stories"];
-                    setStories(data);
-                    console.info(data)
-                } else {
-                    data = await fetchStories();
-                    setStories(data);
-                    console.log(data);
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        getStories();
-    }, [isMyStoriesRoute, user?._id, location.pathname]);
-
-    const getStoriesData = stories.length === 0 ? 'No Stories yet.' 
+    const getStoriesData = stories.length === 0 || !stories ? 'No Stories yet.' 
     : stories.map((story) => <StoryItem key={story._id} story={story}/>);
 
   return (
