0c52519ebdfb96f22726e3130b3047685acf2920
Monica-R <moniroka_339@hotmail.com>
2025-03-12 00:11:10 +0000

    feat: add functionality to fetch user-specific stories in StoryList component

--- src/components/StoryList/StoryList.jsx
+++ src/components/StoryList/StoryList.jsx
@@ -1,25 +1,41 @@
 import './StoryList.css';
 import React from 'react'
 import { useEffect, useState } from 'react'
-import { fetchStories } from '../../services/stories';
+import { fetchStories, fetchStoriesOfUser } from '../../services/stories';
 import StoryItem from '../StoryItem/StoryItem';
+import { useAuth } from '../../context/auth.context';
+import { useLocation } from 'react-router-dom';
 
 function StoryList() {
 
     const [stories, setStories] = useState([]);
+    const { user } = useAuth();
+    const location = useLocation();
+
+    // Determina si estamos en la ruta de "Mis historias"
+    const isMyStoriesRoute = location.pathname.includes('/stories/user');
+
+    console.info('fullpath', location.pathname + location.search + location.hash);
 
     useEffect(() => {
-        getStories();
-    }, []);
-
-    const getStories = async () => {
-        try {
-            const data = await fetchStories();
-            setStories(data);
-        } catch (error) {
-            console.error(error);
+        const getStories = async () => {
+            try {
+                let data;
+                if(isMyStoriesRoute) {
+                    data = user._id ? await fetchStoriesOfUser(user._id) : ["not stories"];
+                    setStories(data);
+                    console.info(data)
+                } else {
+                    data = await fetchStories();
+                    setStories(data);
+                    console.log(data);
+                }
+            } catch (error) {
+                console.error(error);
+            }
         }
-    }
+        getStories();
+    }, [isMyStoriesRoute, user?._id, location.pathname]);
 
     const getStoriesData = stories.length === 0 ? 'No Stories yet.' 
     : stories.map((story) => <StoryItem key={story._id} story={story}/>);
