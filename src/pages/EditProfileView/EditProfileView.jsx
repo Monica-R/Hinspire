3fbc6b12525bfa7ae4cc6d222ef0118e4ec7cd78
Monica-R <moniroka_339@hotmail.com>
2025-03-12 00:11:23 +0000

    feat: implement profile editing and story creation forms in EditProfileView and MyStoriesView components

--- src/pages/EditProfileView/EditProfileView.jsx
+++ src/pages/EditProfileView/EditProfileView.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './EditProfileView.css';
+import { Link } from 'react-router-dom';
 
 function EditProfileView() {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Profile updated');
+  };
+
   return (
-    <div>EditProfileView</div>
+    <div className='edit-profile'>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='username'>Username</label>
+        <input type='text' id='username' name='username' />
+        <label htmlFor='email'>Email</label>
+        <input type='email' id='email' name='email' />
+        <label htmlFor='password'>Password</label>
+        <input type='password' id='password' name='password' />
+        <button type='submit'>Update profile</button>
+      </form>
+      <div className="back-to"><Link to="/profile">Back to profile</Link></div>
+      <div className="danger-zone">
+        <h2>Danger zone</h2>
+        <button className='delete-acc'>Delete account</button>
+      </div>
+    </div>
   )
 }
 
--- src/pages/MyStoriesView/MyStoriesView.jsx
+++ src/pages/MyStoriesView/MyStoriesView.jsx
@@ -1,9 +1,24 @@
 import './MyStoriesView.css';
 import React from 'react'
+import StoryList from '../../components/StoryList/StoryList';
 
 function MyStoriesView() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Story created');
+  };
+
   return (
-    <div>MyStoriesView</div>
+    <section className='story-list'>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title"></label>
+        <input type="text" id="title" name="title" placeholder="Title" />
+        <label htmlFor="description"></label>
+        <textarea id="description" name="description" placeholder="Description"></textarea>
+        <button type="submit">Create story</button>
+      </form>
+      <StoryList />
+    </section>
   )
 }
 
