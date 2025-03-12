a7baf10d017260af82a5a89a8627a28427f447da
Monica-R <moniroka_339@hotmail.com>
2025-03-12 17:51:08 +0000

    feat: implement story fetching functionality in AllStoriesView

--- src/pages/AllStoriesView/AllStoriesView.jsx
+++ src/pages/AllStoriesView/AllStoriesView.jsx
@@ -1,11 +1,27 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { fetchStories } from '../../services/stories';
 import StoryList from '../../components/StoryList/StoryList';
 import './AllStoriesView.css';
-import React from 'react'
 
 function AllStoriesView() {
+
+  const [stories, setStories] = useState([]);
+
+  useEffect(() => {
+    const getStories = async () => {
+      try {
+        const data = await fetchStories();
+        setStories(data);
+      } catch (error) {
+          console.error(error);
+      }
+    }
+      getStories();
+  }, []);
   return (
     <section className='story-list'>
-      <StoryList />
+      <StoryList stories={stories}/>
     </section>
   )
 }
