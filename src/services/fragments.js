2c6f1cae6c9fb8374deec60fb5c403261a8cf3b5
Monica-R <moniroka_339@hotmail.com>
2025-03-12 17:50:34 +0000

    feat: add update, delete, and accept functionality for fragments

--- src/services/fragments.js
+++ src/services/fragments.js
@@ -21,3 +21,42 @@ export const createFragment = async (storyId, fragment, token) => {
         console.error(error);
     }
 };
+
+export const updateFragment = async (fragmentId, fragment, token) => {
+    try {
+        const response = await api.put(`fragments/${fragmentId}`, {content: fragment}, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const deleteFragment = async (fragmentId, token) => {
+    try {
+        const response = await api.delete(`fragments/${fragmentId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const acceptFragment = async (storyId, fragmentId, token) => {
+    try {
+        const response = await api.put(`fragments/${storyId}/confirm/${fragmentId}`, {}, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
