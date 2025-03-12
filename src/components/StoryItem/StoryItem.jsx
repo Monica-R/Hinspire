e3f93a6edc9aac6d5b0b19e2b1ba2eca7a93887a
Monica-R <moniroka_339@hotmail.com>
2025-03-12 17:51:30 +0000

    feat: add update and delete functionality for stories in StoryItem component

--- src/components/StoryItem/StoryItem.jsx
+++ src/components/StoryItem/StoryItem.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../context/auth.context';
 import './StoryItem.css';
+import EditStoryModal from '../EditModal/EditModal';
 
 function StoryItem({story}) {
+  const { authToken, user } = useAuth();
+
+  const handleUpdate = async () => {
+    try {
+      //holaaa
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleDelete = async () => {
+    try {
+      // holaaa
+    } catch (error) {
+     console.error(error); 
+    }
+  };
+
   return (
     <Link to={`/stories/${story._id}`} className="story-card" story={story}>
       <h2 className='story-title'>{ story.title }</h2>
       <p className="story-author">{ story.author.username }</p>
+      { user?._id === story.author._id 
+        && 
+        <div className='options-buttons'>
+          <button className="delete-button" onClick={handleDelete}>Delete story</button>
+          <button className="update-button" onClick={handleUpdate}>Update story</button>
+          <EditStoryModal story={story} token={authToken} /* onUpdate={onUpdateStory} */ />
+        </div> 
+      }
     </Link>
   )
 }
