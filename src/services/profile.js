b03c2163eb39aa857f9f146cafb31083cf235b21
Monica-R <moniroka_339@hotmail.com>
2025-03-13 22:53:14 +0000

    feat: add editProfile and deleteProfile functions to manage user profiles

--- src/services/profile.js
+++ src/services/profile.js
@@ -14,3 +14,34 @@ export const fetchProfile = async () => {
     console.error('error', error);
   }
 }
+
+export const editProfile = async (requestBody, token) => {
+  try {
+    const userRes = await api.put("user/profile", {
+      name: requestBody.name,
+      email: requestBody.email,
+      password: requestBody.password,
+    }, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    });
+    return userRes.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const deleteProfile = async (token) =>{
+  try {
+    const userRes = await api.delete("user/profile", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    return userRes.data;
+  } catch (error) {
+    return error;
+  }
+}
