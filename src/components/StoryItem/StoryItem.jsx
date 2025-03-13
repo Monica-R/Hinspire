8f00f176ca25dc5c95c3f3639145367d0d3e8236
Monica-R <moniroka_339@hotmail.com>
2025-03-13 22:53:29 +0000

    feat: add logging for token in finishStory function to aid debugging

--- src/components/StoryItem/StoryItem.jsx
+++ src/components/StoryItem/StoryItem.jsx
@@ -27,6 +27,7 @@ function StoryItem({story, getStories}) {
 
   const finishStory = async (storyId, token) => {
     try {
+      console.log(token)
       await completeStory(storyId, token);
       getStories();
     } catch (error) {
