a63feafbca1517791595c2c30c141c3386165320
Monica-R <moniroka_339@hotmail.com>
2025-03-17 00:59:47 +0000

    feat: improve styles and content in HomeView with responsive design and new registration section

--- src/pages/HomeView/HomeView.jsx
+++ src/pages/HomeView/HomeView.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './HomeView.css';
 
 function HomeView() {
   return (
     <>
       <section className='section-one'>
-        <div className="layer-container">
+        {/* <div className="layer-container">
           <div className="img-layer img-1"></div>
           <div className="img-layer img-2"></div>
           <div className="img-layer img-3"></div>
           <div className="img-layer img-4"></div>
           <div className="img-layer img-5"></div>
-        </div>
+        </div> */}
         <div className="header__banner">
-          <h1 className="header__h1">Create. Collaborate. Get inspired.</h1>
+          <h1 className="header__h1"><span className='span-1'>Create.</span> Collaborate. <span className='span-2'>Get inspired.</span></h1>
+          <p className='header__p'>
+            Designed to awaken your 
+            creativity, here your ideas come to life 
+            through innovative tools that simplify the 
+            creative process. Connect, share and create 
+            unforgettable stories that transcend borders.
+          </p>
+          <Link className='signup-link sign-up' to="/signup">Let's create →</Link>
+        </div>
+
+        <div className="image-banner">
+          <img src="/images/banner-1.png" alt="banner-1" />
         </div>
 
       </section>
