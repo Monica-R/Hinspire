6e79ae4bbc6122e354610293c9b8565ebb8012b7
Monica-R <moniroka_339@hotmail.com>
2025-03-17 00:58:19 +0000

    feat: add Footer component with responsive styles and social links

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <div className="navbar">
+        <div className="section-left">
+            <h4 className="brand-name">Hinspire</h4>
+            <div className="navbar-buttons">
+                <a href="https://github.com/Monica-R/Hinspire">
+                    <ion-icon className="my-icon" name="logo-github"></ion-icon>
+                </a>
+                <a href="https://www.linkedin.com/in/monica-roka">
+                    <ion-icon className="my-icon" name="logo-linkedin"></ion-icon>
+                </a>
+            </div>
+        </div>
+        <div className="section-right">
+            <ul className='other-options'>
+                <li>Terms of service</li>
+                <li>Privacy Policy</li>
+                <li>FAQ</li>
+            </ul>
+        </div>
+      </div>
+      <div className="info-app">
+        <strong className='footer__span'>&copy; 2025 Hinspire Inc. All rights reserved. Developed with ❤ by <a href="https://github.com/Monica-R">Mónica-R</a></strong>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
