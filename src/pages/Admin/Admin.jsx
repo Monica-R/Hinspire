f620bc0bcf9da2a684a0225ea61b96f990c52b13
Monica-R <moniroka_339@hotmail.com>
2025-03-19 16:23:50 +0000

    feat: add construction notice to Admin panel with updated styling

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Admin() {
   return (
-    <div>Admin</div>
+    <section className='admin-panel' style={{background: 'grey', width: '100%', height: '70vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <h2 className='admin__h2' style={{fontSize: '5rem', fontWeight: '700'}}>
+        ⚠️ 🚧 IN CONSTRUCTION... 🚧 ⚠️
+      </h2>
+    </section>
   )
 }
 
