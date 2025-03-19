6a3eded4f0f9e5a18360e70866d1fe58cdaf0b8c
Monica-R <moniroka_339@hotmail.com>
2025-03-19 16:00:42 +0000

    feat: hamburger menu added in mobile devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../context/auth.context';
 import './Navbar.css'
@@ -6,10 +7,35 @@ import './Navbar.css'
 function Navbar() {
   const { authToken, user, logout } = useAuth();
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false); // Estado para el menú móvil
 
   return (
     <nav className='nav'>
-      { authToken && user ? (
+
+      {/* Botón de menú hamburguesa */}
+      <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+      <div className={`nav-links ${menuOpen ? "open" : ""}`}>
+      {authToken && user ? (
+          <>
+            <Link className="link" to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+            <Link className="link" to="/stories" onClick={() => setMenuOpen(false)}>Stories</Link>
+            <Link className="link" to="#" onClick={() => setMenuOpen(false)}>About</Link>
+            <Link className="link" to="/profile" onClick={() => setMenuOpen(false)}>Hi, {user?.username}!</Link>
+            <button className="logout" onClick={() => { logout(); navigate("/"); setMenuOpen(false); }}>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link className="link" to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+            <Link className="link" to="/stories" onClick={() => setMenuOpen(false)}>Stories</Link>
+            <Link className="link" to="#" onClick={() => setMenuOpen(false)}>About</Link>
+            <Link className="link login" to="/login" onClick={() => setMenuOpen(false)}>Login</Link>
+            <Link className="link signup" to="/signup" onClick={() => setMenuOpen(false)}>SignUp</Link>
+          </>
+        )}
+      </div>
+{/*       { authToken && user ? (
         <>
           <Link className=' link' to="/">Home</Link>
           <Link className='link' to="/stories">Stories</Link>
@@ -25,7 +51,7 @@ function Navbar() {
           <Link className=' link login' to="/login">Login</Link>
           <Link className=' link signup' to="/signup">SignUp</Link>
         </>
-      ) }
+      ) } */}
     </nav>
   )
 }
