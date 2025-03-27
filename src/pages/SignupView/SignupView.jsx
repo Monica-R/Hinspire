8d3d15756b0219f27922f12f8baf1ae75fa1fd9a
Monica-R <moniroka_339@hotmail.com>
2025-03-27 18:27:58 +0000

    feat: add success and error notifications for login and signup processes

--- src/pages/SignupView/SignupView.jsx
+++ src/pages/SignupView/SignupView.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
 import { signup } from '../../services/auth';
 import { useAuth,  } from '../../context/auth.context';
 import './SignupView.css';
@@ -26,6 +27,8 @@ function SignupView() {
       startLoading();
       // Llamamos a la función signup del servicio
       const data = await signup({ email, password, username });
+      toast.success("Signup successful!");
+      // Si el usuario se ha registrado correctamente, guardamos el token en el contexto
 
       if (data && data.authToken) {
         setAuthToken(data.authToken);
@@ -34,6 +37,8 @@ function SignupView() {
       }
     } catch (error) {
       console.error(error);
+      const errorMessage = error.data?.message || "Server error, please try again.";
+      toast.error(errorMessage);
     } finally {
       stopLoading();
     }
