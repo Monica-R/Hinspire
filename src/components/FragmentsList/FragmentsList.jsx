6097dc3284ef77a3de887d7004f970c0f0b457c8
Monica-R <moniroka_339@hotmail.com>
2025-03-27 16:10:57 +0000

    feat: add styling for FragmentsList component and enhance layout with date and author display

--- src/components/FragmentsList/FragmentsList.jsx
+++ src/components/FragmentsList/FragmentsList.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-
+import './FragmentList.css'
 function FragmentsList({fragments}) {
   return (
     <article className='acepted-fragments'>
       { fragments.length > 0 ?
         <div>
-          { fragments.map((fragment) => (
-            <div key={fragment._id}>
-              <p>{fragment.content}</p>
-              <p>{fragment.author.username}</p>
-            </div>
-          ))}
+          { fragments.map((fragment) => {
+            const formattedDate = new Date(fragment.createdAt).toLocaleDateString();
+            return (
+              <div key={fragment._id} className='fragment-item'>
+                <span className='fragment-date'>{formattedDate}</span>
+                <span className='fragment-item-author'>{fragment.author.username}</span>
+                <p className='fragment-content'>{fragment.content}</p>
+              </div>
+            )
+          })}
         </div>
         : <p>No fragments yet.</p>
       }
